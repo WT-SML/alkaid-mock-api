c2baae83f5c232fc8f7c0eca02240feb5d746c55
wt-sml <809189656@qq.com>
2020-09-09 17:11:30 +0800

    新增bdr mock数据接口

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ if (!(process.env.NODE_ENV === 'production')) {
   app.use(staticFiles('/static/', __dirname + '/static'))
 }
 app.use(cors({
-  origin: () => ['http://adm.test.shangshuyixue.com'], // 跨域
+  origin: () => ['http://www.test.bdr.com'], // 跨域 'http://adm.test.shangshuyixue.com','http://www.test.shangshuyixue.com','http://www.test.bdr.com',
   maxAge: 5, //指定本次预检请求的有效期，单位为秒。
   credentials: true, //是否允许发送Cookie
   allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法
--- controllers/index.js
+++ controllers/index.js
@@ -17,7 +17,7 @@ module.exports = {
   'GET /yxhk/adm/training': async (ctx, next) => {
     ctx.response.body = createListRes({
       'id|+1': 1,
-      'hospital': '@cname',
+      'hospital|1': ['四川大学华西第二医院', '山东省齐鲁医院', '单县中心医院'],
       'create_at': '@date("yyyy-MM-dd")',
       'province|1': [11, 12, 13, 14, 15, 21, 22, 23, 31, 32, 33, 34, 35, 36, 37],
       'type': [0, 1],
@@ -26,7 +26,7 @@ module.exports = {
       'sign_up_end': '@date("yyyy-MM-dd")',
       'base_salary|900-5000': 1,
       'url': 'https://cn.vuejs.org/v2/guide/',
-      'image': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598851578781&di=6346970eb9ddca41e48f2e74fce95a44&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201604%2F17%2F20160417100243_5nLvB.jpeg',
+      'image': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598875539380&di=a5e2695e3be708b283fa87bb6418d8fd&imgtype=0&src=http%3A%2F%2Fg.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Fb21c8701a18b87d6993fcf52010828381f30fd7a.jpg',
       'desc|1-3': '我是一个简介',
     })
   },
@@ -59,14 +59,14 @@ module.exports = {
       'create_at': '@date("yyyy-MM-dd")',
       'cmt|5-10': '测试评论',
       'status|1': [0, 1],
-      'image1': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598851578781&di=6346970eb9ddca41e48f2e74fce95a44&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201604%2F17%2F20160417100243_5nLvB.jpeg',
-      'image2': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598851578781&di=6346970eb9ddca41e48f2e74fce95a44&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201604%2F17%2F20160417100243_5nLvB.jpeg',
-      'image3': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598851578781&di=6346970eb9ddca41e48f2e74fce95a44&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201604%2F17%2F20160417100243_5nLvB.jpeg',
+      'image1': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598875539380&di=a5e2695e3be708b283fa87bb6418d8fd&imgtype=0&src=http%3A%2F%2Fg.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Fb21c8701a18b87d6993fcf52010828381f30fd7a.jpg',
+      'image2': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598875539380&di=a5e2695e3be708b283fa87bb6418d8fd&imgtype=0&src=http%3A%2F%2Fg.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Fb21c8701a18b87d6993fcf52010828381f30fd7a.jpg',
+      'image3': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598875539380&di=a5e2695e3be708b283fa87bb6418d8fd&imgtype=0&src=http%3A%2F%2Fg.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Fb21c8701a18b87d6993fcf52010828381f30fd7a.jpg',
     })
   },
-    /** 
-   * 获取adm规培
-   */
+  /** 
+ * 获取adm规培
+ */
   'GET /yxhk/adm/training/:id': async (ctx, next) => {
     ctx.response.body = Mock.mock({
       'id': ctx.params.id,
@@ -79,7 +79,7 @@ module.exports = {
       'sign_up_end': '@date("yyyy-MM-dd")',
       'base_salary|900-5000': 1,
       'url': 'https://cn.vuejs.org/v2/guide/',
-      'image': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598851578781&di=6346970eb9ddca41e48f2e74fce95a44&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201604%2F17%2F20160417100243_5nLvB.jpeg',
+      'image': 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598875539380&di=a5e2695e3be708b283fa87bb6418d8fd&imgtype=0&src=http%3A%2F%2Fg.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Fb21c8701a18b87d6993fcf52010828381f30fd7a.jpg',
       'desc|1-3': '我是一个简介',
     })
   },
@@ -90,5 +90,112 @@ module.exports = {
     ctx.response.body = {
       id: ctx.params.id
     }
+  },
+  /** 
+   * 获取规培列表
+   */
+  'GET /yxhk/training': async (ctx, next) => {
+    ctx.response.body = createListRes({
+      'id|+1': 1,
+      'hospital|1': ['四川大学华西第二医院', '山东省齐鲁医院', '单县中心医院'],
+      'create_at': '@date("yyyy-MM-dd")',
+      'province|1': [11, 12, 13, 14, 15, 21, 22, 23, 31, 32, 33, 34, 35, 36, 37],
+      'type': [0, 1],
+      'status|1': [0, 1],
+      'sign_up_start': '@date("yyyy-MM-dd")',
+      'sign_up_end': '@date("yyyy-MM-dd")',
+      'base_salary|900-5000': 1,
+      'url': 'https://cn.vuejs.org/v2/guide/',
+      'image|1': ['https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598875539380&di=a5e2695e3be708b283fa87bb6418d8fd&imgtype=0&src=http%3A%2F%2Fg.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2Fb21c8701a18b87d6993fcf52010828381f30fd7a.jpg', null],
+      'desc|1-3': '我是一个简介',
+      'cmts_count|50-900': 1,
+      'top_count|50-900': 1,
+    })
+  },
+  /** 
+   * bdr pc微信登录
+   */
+  'POST /bdr/user/login': async (ctx, next) => {
+    ctx.response.body = {
+      user: {
+        id: 10004,
+        name: '芜桐',
+        avatar: 'https://www.sumoli.com/img/logo_01.91a175d3.png'
+      },
+      csrf_token: 'csrf_token',
+    }
+  },
+  /** 
+   * bdr 用户收藏夹
+   */
+  'GET /bdr/user/:id/favorite': async (ctx, next) => {
+    ctx.response.body = createListRes({
+      'id|+1': 1,
+      'name|1': ['开发', '前端', '后端', 'UI设计', '软件测试', '实用工具'],
+      'user_id|1': [10004, 10005, 10006, 10007],
+      'type|1': [0, 1], // 0私人 1共享
+    })
+  },
+  /** 
+   * bdr 全部收藏夹
+   */
+  'GET /bdr/favorite': async (ctx, next) => {
+    ctx.response.body = createListRes({
+      'id|+1': 1,
+      'name|1': ['开发', '前端', '后端', 'UI设计', '软件测试', '实用工具'],
+      'cate': ['js', 'java', 'python', 'nodejs', 'vue', 'react', '颜色', '金融'], // 0私人 1共享 2关注
+      'desc|1-3': '我是一个简介',
+      'website_count|1-50': 1,
+      'fans_count|1-50000': 1,
+      'user_id|1': [10004, 10005, 10006, 10007],
+      'type': 1,
+    })
+  },
+  /** 
+   * bdr 收藏夹详情
+   */
+  'GET /bdr/favorite/:id': async (ctx, next) => {
+    ctx.response.body =
+      Mock.mock({
+        'id': ctx.params.id,
+        'name|1': ['开发', '前端', '后端', 'UI设计', '软件测试', '实用工具'],
+        'cate|10': [
+          { 'id|+1': 1, 'name|1': ['js', 'java', 'python', 'nodejs', 'vue', 'react', '金融', '颜色'], },
+        ],
+        'desc|1-3': '我是一个简介',
+        'website_count|1-50': 1,
+        'fans_count|1-50000': 1,
+        'user_id|1': [10004, 10005, 10006, 10007],
+        'user_avatar|1': ['https://static.iiter.cn/weixin.ico', 'https://static.iiter.cn/react.png', 'https://static.iiter.cn/gitmind.png', 'https://static.iiter.cn/163.ico'],
+        'user_name': '@cname',
+        'type|1': [0, 1], // 0私人 1共享
+        'cate_id|1-10': 1,
+        'profile|1-3': '设计师必备站点，国内优秀设计师都在上面。设计师必备站点，国内优秀设计师都在上面。设计师必备站点，国内优秀设计师都在上面。设计师必备站点，国内优秀设计师都在上面。',
+        'attention|1': [0, 1]
+      })
+  },
+  /** 
+   * bdr 更新收藏夹
+   */
+  'PUT /bdr/favorite/:id': async (ctx, next) => {
+    ctx.response.body = {
+      msg: 'ok'
+    }
+  },
+  /** 
+   * bdr 获取收藏夹内urls
+   */
+  'GET /bdr/favorite/:id/urls': async (ctx, next) => {
+    ctx.response.body = Mock.mock({
+      'data|17': [
+        {
+          'name|1': ['芜桐博客', '艾特网', '知乎', '阿里云', '腾讯云'],
+          'id|+1': 1,
+          'url|1': ['https://sumoli.com', 'https://iiter.cn/'],
+          'logo|1': ['https://static.iiter.cn/weixin.ico', 'https://static.iiter.cn/react.png', 'https://static.iiter.cn/gitmind.png', 'https://static.iiter.cn/163.ico'],
+          'desc|1-3': '凤凰鸣矣，于彼高岗。芜桐生矣，于彼朝阳。'
+        }
+      ],
+    })
   },
 };
